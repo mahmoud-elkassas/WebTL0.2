f447032b000a483ec2531060f2bc1e1b05006b0a
mahmoud <105293110+mahmoud-elkassas@users.noreply.github.com>
2025-07-18 03:37:30 +0200

    Remove invalid showRetry and onRetry props from ConsolidatedReviewModal

--- components/translation/translation-form.tsx
+++ components/translation/translation-form.tsx
@@ -2181,10 +2181,6 @@ export function TranslationForm({ series, onTranslate }: TranslationFormProps) {
         })()}
         chapterMemory={chapterMemory}
         onComplete={handleConsolidatedReviewComplete}
-        showRetry={true}
-        onRetry={() => {
-          setHasFailed(false);
-          processTranslationWithRetry(0);
         }}
       />
     </div>
